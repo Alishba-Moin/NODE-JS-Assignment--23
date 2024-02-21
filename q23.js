24a96bdf81598bdc6a8edea5e1ad7fe4d20d373a
Alishba-Moin <159716625+Alishba-Moin@users.noreply.github.com>
2024-02-21 06:16:04 -0800

    Add files via upload

---
+++ q23.js
@@ -0,0 +1,27 @@
+"use strict";
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True Otherwise I Predict False.");
+console.log(car == 'subaru');
+console.log(car == 'audi');
+let Name = 'Alishba';
+console.log("Is Name == 'Alishba'? I predict True otherwise I Predict False.");
+console.log(Name == 'Alishba');
+console.log(Name == 'Lishaa');
+let Age = 18;
+console.log("Is age == 18 ? I predict True otherwise I Predict False.");
+console.log(Age === 18);
+console.log(Age === "18");
+let Class = '10th';
+console.log("Is class == '10th' ? I predict True otherwise I Predict False.");
+console.log(Class === '10th');
+console.log(Class === '8th');
+let Rollno = 14890;
+console.log("Is rollno == '14890' ? I predict True otherwise I Predict False.");
+console.log(Rollno == 14890);
+console.log(Rollno == 14891);
